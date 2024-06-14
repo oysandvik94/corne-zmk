3055e8823bea72fb01b107e4f7a5981c82a5a1ed
oysandvik94 <oysandvik94@gmail.com>
2024-06-14 08:49:22 +0200

    mindre tap term

--- flash.sh
+++ flash.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+set -e 
+
 if [ "$#" -ne 1 ]; then
 	echo "Usage: $0 <left|right>"
 	echo "Will flash the given side of the keyboard"
 	exit 1
 fi
 
-FILENAME
 UUID=0042-0042
 case $1 in
 left)
-	FILENAME="corne_left-nice_nano_v2-zmk.uf2"
+	FILENAME="corne_left nice_view_adapter nice_view-nice_nano_v2-zmk.uf2"
+	# FILENAME="settings_reset-nice_nano_v2-zmk.uf2"
 	;;
 right)
-	FILENAME="corne_right-nice_nano_v2-zmk.uf2"
+	FILENAME="corne_right nice_view_adapter nice_view-nice_nano_v2-zmk.uf2"
+	# FILENAME="settings_reset-nice_nano_v2-zmk.uf2"
 	;;
 *)
 	# Handle unexpected arguments
@@ -27,6 +30,6 @@ unzip $HOME/Downloads/firmware.zip -d /tmp/firmware
 
 sudo mount UUID=$UUID /media/keyboard
 
-sudo cp /tmp/firmware/$FILENAME /media/keyboard/
+sudo cp /tmp/firmware/"$FILENAME" /media/keyboard/
 
 rm -rf /tmp/firmware
